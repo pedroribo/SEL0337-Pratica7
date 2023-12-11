b501def1785997803ed7f6ffa4c9d298dd7ce4fa
pedroribo <pedrorb@usp.br>
2023-12-11 20:55:20 +0000

    Envio dos arquivos da prática 7.

---
+++ ledPWM.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+GPIO.setup(23, GPIO.OUT)
+pwm = GPIO.PWM(23, 100)
+pwm.start(0)
+
+while(True):
+	for dc in range(101):
+    		pwm.ChangeDutyCycle(dc)
+    		sleep(0.01)
